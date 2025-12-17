f346930b1b8c0c75949ace41d83e10aa0f7fa5c3
ArtefactumSK <artefactum@artefactum.sk>
2025-12-17 19:28:34 +0100

    CITY

--- includes/frontend/schedules-shortcode.php
+++ includes/frontend/schedules-shortcode.php
@@ -10,7 +10,15 @@
 
 if (!defined('ABSPATH')) exit;
 
-add_shortcode('spa_schedules', function () {
+add_shortcode('spa_schedules', function ($atts) {
+    $atts = shortcode_atts([
+        'city' => '',
+    ], $atts);
+
+    if ($atts['city']) {
+        $_GET['city'] = $atts['city'];
+    }
+
     ob_start();
     include __DIR__ . '/schedules-list.php';
     return ob_get_clean();
