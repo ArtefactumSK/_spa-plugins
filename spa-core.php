c9bbefe55bc47c17df2b0d7072ae834d1a01b810
ArtefactumSK <artefactum@artefactum.sk>
2025-12-17 10:43:29 +0100

    START

---
+++ spa-core.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: SPA Core
+ * Description: Core systém pre Piasecky Academy (DB, logika, integrácie).
+ * Version: 1.0.0
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// DB install
+require_once __DIR__ . '/includes/db/install.php';
+
+register_activation_hook(__FILE__, 'spa_core_install_db');
